03dce4e0e5c1e3dc2154dca5661a0fdd04bf8b96
maHolgersen <37177875+maHolgersen@users.noreply.github.com>
2018-10-12 11:35:24 +0200

    added primary and foreign keys

--- assignment1.sql
+++ assignment1.sql
@@ -5,16 +5,21 @@ SET FOREIGN_KEY_CHECKS=0;
 
 
 CREATE TABLE College (
+	college_ID INT NOT NULL,
     college_name VARCHAR(50) NOT NULL UNIQUE,
     college_office INT,
-    college_phone INT(8)
+    college_phone INT(8),
+    PRIMARY KEY (college_ID)
 );
     
 CREATE TABLE Academic_Department (
     department_name VARCHAR(50) NOT NULL UNIQUE,
     code_number INT NOT NULL UNIQUE,
     department_office INT,
-    department_phone INT(8)
+    department_phone INT(8),
+    college_ID INT,
+    PRIMARY KEY (code_number),
+    FOREIGN KEY (college_ID) REFERENCES College(college_ID)
 );
     
 CREATE TABLE Courses (
@@ -22,7 +27,10 @@ CREATE TABLE Courses (
     code_number INT NOT NULL UNIQUE,
     course_level INT,
     credit_hours INT,
-    description VARCHAR(255)
+    description VARCHAR(255),
+    department_code_number INT,
+    PRIMARY KEY (code_number),
+    FOREIGN KEY (department_code_number) REFERENCES Academic_Department(code_number)
 );
     
 CREATE TABLE Instructor (
@@ -30,17 +38,25 @@ CREATE TABLE Instructor (
     instructor_name VARCHAR(50),
     instructor_office INT,
     instructor_phone INT(8),
-    instructor_rank INT
+    instructor_rank INT,
+    department_code_number INT,
+    PRIMARY KEY (instructor_ID),
+    FOREIGN KEY (department_code_number) REFERENCES Academic_Department(code_number)
 );
     
-CREATE TABLE student (
+CREATE TABLE Student (
     f_name VARCHAR(25),
     l_name VARCHAR(25),
     student_ID INT NOT NULL UNIQUE,
     student_address VARCHAR(50),
     student_phone INT,
     major_code INT,
-    date_of_birth DATE
+    date_of_birth DATE,
+    department_code_number INT,
+    course_code_number INT,
+    PRIMARY KEY (student_ID),
+    FOREIGN KEY (course_code_number) REFERENCES Courses(code_number),
+    FOREIGN KEY (department_code_number) REFERENCES Academic_Department(code_number)
 );
     
 CREATE TABLE Course_Section (
@@ -48,9 +64,38 @@ CREATE TABLE Course_Section (
     section_number INT,
     semester VARCHAR(25),
     section_year YEAR,
-    day_time DATETIME
+    instructor_ID INT,
+    course_code_number INT,
+    PRIMARY KEY (section_ID),
+    FOREIGN KEY (instructor_ID) REFERENCES Instructor(instructor_ID),
+    FOREIGN KEY (course_code_number) REFERENCES Courses(code_number)
+);
+
+CREATE TABLE Grade(
+	grade VARCHAR (2),
+    student_ID INT,
+    section_ID INT,
+    FOREIGN KEY (student_ID) REFERENCES Student(student_ID),
+    FOREIGN KEY (section_ID) REFERENCES Course_Section(section_ID)
+);
+
+CREATE TABLE Classroom(
+	department_code_number INT,
+    classroom_number INT,
+    section_ID INT,
+    FOREIGN KEY (department_code_number) REFERENCES Academic_Department(code_number),
+    FOREIGN KEY (section_ID) REFERENCES Course_Section(section_ID)
+    
 );
     
+CREATE TABLE Class_Schedule(
+	class_day CHAR(3),
+    class_time INT,
+    section_ID INT,
+    FOREIGN KEY (section_ID) REFERENCES Course_Section(section_ID),
+    CHECK (class_day = 'mon' OR 'tue' OR 'wen' OR 'tur' OR 'fri'),
+    CHECK (class_time BETWEEN 0000 AND 2359)
+);
     
     
 SET FOREIGN_KEY_CHECKS=1;
